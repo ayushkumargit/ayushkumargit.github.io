29cfb075964004a1d5fb5f048b987c3c766f3b07
ayushkumargit <103486918+ayushkumargit@users.noreply.github.com>
2022-04-22 13:50:55 +0530

    title
    title of note

--- noteApp/project1.js
+++ noteApp/project1.js
@@ -6,15 +6,21 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let obj = {
+    text:addTxt.value,
+    title:addTitle.value
+  }
+  notesObj.push(obj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
 //   console.log(notesObj);
   showNotes();
 });
@@ -31,10 +37,10 @@ function showNotes() {
   let html = "";
   notesObj.forEach(function(element, index) {
     html += `
-            <div class="noteCard my-2 mx-2 card" style="width: 20rem;">
+            <div class="noteCard my-2 mx-2 card" style="width: 20rem;" >
                     <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
+                        <h5 class="card-title"> ${element.title}</h5>
+                        <p class="card-text"> ${element.text}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                       
                     </div>
@@ -52,7 +58,7 @@ function showNotes() {
 
 function deleteNote(index) {
 
-  console.log('i am deleting note no ' , index);
+  // console.log('i am deleting note no ' , index);
 
   let notes = localStorage.getItem("notes");
   if (notes == null) {
